a169b18403a3cd57ca3c84d6a912230e83002394
marvelnano <marvelnano@gmail.com>
2024-09-30 08:46:29 -0500

    se agregó endpoints de cursos al proyecto

---
+++ course-service/src/main/java/com/ejemplo/course_service/controller/MiControlador.java
@@ -0,0 +1,41 @@
+package com.ejemplo.course_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "Bienvenida", description = "Controlador mensaje de bienvenida")
+public class MiControlador {
+
+    @GetMapping("/saludo")
+    @Operation(
+        summary = "Saludo",
+        description = "Mensaje de bienvenida que puede ser usando en el FrontEnd",
+        tags = {"Bienvenida"},
+        requestBody = @RequestBody(
+            description = "Saludo de Bienvenidad",
+            required = false,
+            content = @Content(
+                mediaType = "application/json"
+            )
+        ),
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Successful message",
+                content = @Content(
+                    mediaType = "application/json"
+                )
+            )
+        }
+    )
+    public String saludo() {
+        return "Hola, mundo - Cursos!!";
+    }
+}
---
+++ course-service/src/main/java/com/ejemplo/course_service/model/entity/CourseEntity.java
@@ -0,0 +1,35 @@
+package com.ejemplo.course_service.model.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "courses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class CourseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_course", nullable = false)
+    private Long idCurso;
+
+    private String nombre;
+
+    @Column(name = "horas_teoria", nullable = false)
+    private Long horasTeoria;
+
+    @Column(name = "horas_practica", nullable = false)
+    private Long horasPractica;
+
+    private String profesor;
+
+    private static final long serialVersionUID = 6690689988058914671L;
+    
+}
